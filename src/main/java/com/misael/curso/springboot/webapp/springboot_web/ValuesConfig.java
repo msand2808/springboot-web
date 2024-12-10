bca3d7b37f5929268c5ba0d34faf13733ea125b8
Misael <misael.sandoval2808@gmail.com>
2024-12-10 14:29:03 -0600

    Se hizo un pequeño cambio en ValuesConfig, se agrego el encodigo UTF-8 para que permita utilizar ciertos caracteres especiales

--- src/main/java/com/misael/curso/springboot/webapp/springboot_web/ValuesConfig.java
+++ src/main/java/com/misael/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -2,12 +2,17 @@ package com.misael.curso.springboot.webapp.springboot_web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 /*
  * Configurando las propiedades personalizadas de el proyecto, con @PropertySources se pueden configurar mas
  */
-@PropertySource("classpath:values.properties")
+@PropertySources(
+    {
+        @PropertySource(value="classpath:values.properties", encoding = "UTF-8")
+    })
+
 public class ValuesConfig {
 
 }
